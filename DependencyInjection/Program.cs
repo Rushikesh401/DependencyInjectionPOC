37811796c44e266325c07ce28eec75ddbebf4bbf
Rushikesh Suradkar <rushikeshsuradkar2000@gmail.com>
2024-03-26 23:48:28 +0530

    tight coupling Service class is dependent on Repository Class

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using DependencyInjection;
+
+class Program
+
+{
+    static void Main(String[] strings)
+    { 
+        Service service = new Service();
+        service.Execute();
+    }
+
+}
+
---
+++ DependencyInjection/Service.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class Service
+    {
+        private Repository _repository;
+        public Service()
+        {
+            // tight coupling with the Respository class
+            _repository = new Repository();
+            
+            //when tightly coupled, any changes made directly affect to dependent class. 
+            //Here if any chnages made in Repository 
+            //class will affect implementation of Service class
+
+
+        }
+
+        public void Execute()
+        {
+            _repository.GetData();
+            Console.WriteLine("Service Executed");
+        }
+
+    }
+}
