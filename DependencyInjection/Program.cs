5fc7d0e3dc82612e66ff24f1e28dec6f28bf2774
Rushikesh Suradkar <rushikeshsuradkar2000@gmail.com>
2024-03-27 00:37:55 +0530

    Inversion of Control - Using DI container
    Used default container provided by microsoft.extension.DependencyInjection

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -1,15 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 
 class Program
 
 {
     static void Main(String[] strings)
-    { 
-        IRepository repository = new Repository();
-        Service service = new Service(repository);
+    {
+        // Setup DI container
+        var serviceProvider = new ServiceCollection()
+            .AddTransient<IRepository, Repository>()
+            .AddTransient<Service>()
+            .BuildServiceProvider();
+
+
+        // Resolve Service from the container
+        var service = serviceProvider.GetRequiredService<Service>();
         service.Execute();
+
+
+        /* Using DI container above we eliminated need of  creating and managing 
+        the instace of Repostory, it all handled by DI container
+         */
+
+        //IRepository repository = new Repository();
+        //Service service = new Service(repository);
+
     }
 
 }
